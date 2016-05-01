763bc7667558e56b74ff1e09225091e366347962
AJ Schrier <ajschrier@gmail.com>
2016-04-30 17:06:17 -0700

    Created project

---
+++ SMCollectorMac/SMCollectorMac/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SMCollectorMac
+//
+//  Created by AJ Schrier on 4/30/16.
+//  Copyright © 2016 AJ Schrier. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ SMCollectorMac/SMCollectorMac/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SMCollectorMac
+//
+//  Created by AJ Schrier on 4/30/16.
+//  Copyright © 2016 AJ Schrier. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
